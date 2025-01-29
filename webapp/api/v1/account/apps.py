4ecc41068c4b9a5188399cf6a2bf15e762c8f808
shinkeonkim <singun11@kookmin.ac.kr>
2025-01-29 20:55:16 +0900

    refactor: account app으로 변경

--- webapp/api/v1/account/apps.py
+++ webapp/api/v1/account/apps.py
@@ -2,4 +2,4 @@ from django.apps import AppConfig
 
 
 class ApiV1UserConfig(AppConfig):
-    name = 'api.v1.user'
+    name = "api.v1.account"
--- webapp/api/v1/account/urls.py
+++ webapp/api/v1/account/urls.py
@@ -1,13 +1,14 @@
 from django.urls import path
 
-app_name = 'api.v1.user'
+app_name = "api.v1.account"
 
 from django.http import HttpResponse
 
+
 def test_view(request):
-    return HttpResponse('Test view')
+    return HttpResponse("Test view")
 
 
 urlpatterns = [
-    path('test/', test_view, name='test'),
+    path("test/", test_view, name="test"),
 ]
