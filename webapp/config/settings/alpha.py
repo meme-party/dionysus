427567343d616b83caeb47c79a4a523e74d5293a
shinkeonkim <dev.shinkeonkim@gmail.com>
2025-03-11 20:08:41 +0900

    feat: alpha image build workflow

--- webapp/config/settings/alpha.py
+++ webapp/config/settings/alpha.py
@@ -1,3 +1,26 @@
 from .base import *  # noqa: F401, F403
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["alpha-api.memez.party", "memez.party"]
+
+DEBUG = False
+
+DATABASES = {
+    "default": {
+        "ENGINE": "psqlextra.backend",
+        "NAME": env("POSTGRES_DB"),
+        "USER": env("POSTGRES_USER"),
+        "PASSWORD": env("POSTGRES_PASSWORD"),
+        "HOST": "db",
+        "PORT": env("POSTGRES_PORT"),
+    }
+}
+
+# Static files (CSS, JavaScript, Images)
+
+STATIC_DIR = f"{BASE_DIR}/static"
+STATIC_ROOT = f"{BASE_DIR}/static"
+STATIC_URL = "/static/"
+
+# Media files
+MEDIA_ROOT = f"{BASE_DIR}/media"
+MEDIA_URL = "media/"
