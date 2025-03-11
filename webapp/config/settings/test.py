cd25b1166b24ec78b6d0516f0c7f0a98fc02a028
shinkeonkim <dev.shinkeonkim@gmail.com>
2025-03-11 19:56:43 +0900

    feat: CI workflow 추가

---
+++ webapp/config/settings/test.py
@@ -0,0 +1,22 @@
+from .base import *  # noqa: F401, F403
+
+DATABASES = {
+    "default": {
+        "ENGINE": "psqlextra.backend",
+        "NAME": env("POSTGRES_DB"),
+        "USER": env("POSTGRES_USER"),
+        "PASSWORD": env("POSTGRES_PASSWORD"),
+        "HOST": "localhost",
+        "PORT": env("POSTGRES_PORT"),
+    }
+}
+
+# Static files (CSS, JavaScript, Images)
+
+STATIC_DIR = f"{BASE_DIR}/static"
+STATIC_ROOT = f"{BASE_DIR}/static"
+STATIC_URL = "/static/"
+
+# Media files
+MEDIA_ROOT = f"{BASE_DIR}/media"
+MEDIA_URL = "media/"
