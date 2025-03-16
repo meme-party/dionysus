452c2f6a5c2583236f57d22ef39395d31cf61468
shinkeonkim <singun11@kookmin.ac.kr>
2025-03-16 19:52:00 +0900

    fix: CSRF_TRUSTED_ORIGINS

--- webapp/config/settings/alpha.py
+++ webapp/config/settings/alpha.py
@@ -1,6 +1,7 @@
 from .base import *  # noqa: F401, F403
 
 ALLOWED_HOSTS = ["alpha-api.memez.party", "memez.party"]
+CSRF_TRUSTED_ORIGINS = ["https://alpha-api.memez.party", "https://memez.party"]
 
 DEBUG = False
 
