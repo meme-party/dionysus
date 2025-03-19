4d1d8c9381a6c5f95edee951ad56dcba8d3039b0
shinkeonkim <dev.shinkeonkim@gmail.com>
2025-03-19 14:37:12 +0900

    feat: Meme의 thumbnail link에 대해 full path가 나오도록함.

--- webapp/api/v1/file_manager/serializers/thumbnail_serializer.py
+++ webapp/api/v1/file_manager/serializers/thumbnail_serializer.py
@@ -1,4 +1,5 @@
 from file_manager.models import Thumbnail
+from rest_framework import serializers
 from rest_framework.serializers import ModelSerializer
 
 
@@ -6,3 +7,8 @@ class ThumbnailSerializer(ModelSerializer):
     class Meta:
         model = Thumbnail
         fields = ["url"]
+
+    url = serializers.SerializerMethodField("get_url")
+
+    def get_url(self, obj):
+        return self.context["request"].build_absolute_uri(obj.url)
