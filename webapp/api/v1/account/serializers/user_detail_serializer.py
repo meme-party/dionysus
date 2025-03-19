37702c15c073b8134ebbdc3553ce5617d6ef2055
shinkeonkim <dev.shinkeonkim@gmail.com>
2025-03-19 13:32:21 +0900

    feat: /api/v1/accounts/user/ 상세 정보에 provider 정보를 제공

---
+++ webapp/api/v1/account/serializers/user_detail_serializer.py
@@ -0,0 +1,16 @@
+from dj_rest_auth.serializers import UserDetailsSerializer
+
+from .social_account_serializer import SocialAccountSerializer
+
+
+class UserDetailSerializer(UserDetailsSerializer):
+    """
+    UserSerializer를 확장하여 소셜 계정 정보를 포함
+    """
+
+    social_account = SocialAccountSerializer(
+        source="socialaccount_set", many=True, read_only=True
+    )
+
+    class Meta(UserDetailsSerializer.Meta):
+        fields = UserDetailsSerializer.Meta.fields + ("social_account",)
--- webapp/api/v1/account/urls.py
+++ webapp/api/v1/account/urls.py
@@ -1,11 +1,13 @@
-from django.urls import path
+from django.urls import path, re_path
 
 from .views import KakaoLogin, kakao_callback, kakao_login
+from .views.user_detail_view import UserDetailView
 
 app_name = "api.v1.account"
 
 
 urlpatterns = [
+    re_path(r"^accounts/user/?$", UserDetailView.as_view(), name="rest_user_details"),
     path("accounts/kakao/login/", kakao_login, name="kakao_login"),
     path("accounts/kakao/login/callback/", kakao_callback, name="kakao_callback"),
     path(
