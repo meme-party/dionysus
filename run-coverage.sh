43d8120ee1d3acbde05f6eb4a87196134cb1489f
shinkeonkim <singun11@kookmin.ac.kr>
2025-03-28 21:10:07 +0900

    feat: coverage report를 github 상에서 확인할 수 있도록 workflow 추가

--- run-coverage.sh
+++ run-coverage.sh
@@ -9,4 +9,5 @@ docker-compose exec dionysus python -m coverage report -m
 # HTML 형식의 상세 리포트 생성
 docker-compose exec dionysus python -m coverage html
 
-echo "Coverage report generated in coverage_html_report/ directory"
+echo "Coverage report generated in webapp/htmlcov/ directory"
+echo "Open webapp/htmlcov/index.html to view the report"
