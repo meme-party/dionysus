0e19eab1bc63a835aa126f269df004d6355bc7f6
shinkeonkim <singun11@kookmin.ac.kr>
2025-03-28 19:41:55 +0900

    feat: 인기도 정보에 따른 정렬 추가

--- webapp/api/v1/tag/serializers.py
+++ webapp/api/v1/tag/serializers.py
@@ -10,7 +10,17 @@ class TagCategorySerializer(serializers.ModelSerializer):
 
 class TagSerializer(serializers.ModelSerializer):
     category = TagCategorySerializer()
+    popularity = serializers.FloatField(read_only=True, required=False)
+    memes_count = serializers.IntegerField(read_only=True, required=False)
+    bookmarkings_count = serializers.IntegerField(read_only=True, required=False)
 
     class Meta:
         model = Tag
-        fields = ["id", "name", "category"]
+        fields = [
+            "id",
+            "name",
+            "category",
+            "popularity",
+            "memes_count",
+            "bookmarkings_count",
+        ]
