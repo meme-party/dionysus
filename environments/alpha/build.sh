35159dfd478315997bd24dfe100abad59aea807b
shinkeonkim <singun11@kookmin.ac.kr>
2025-03-28 23:49:00 +0900

    refactor: github action caching

--- environments/alpha/build.sh
+++ environments/alpha/build.sh
@@ -2,7 +2,12 @@
 set -e
 
 echo "Building Docker image: ${DOCKER_IMAGE} with PORT ${PORT}"
-docker build --build-arg PORT=${PORT} -t "${DOCKER_IMAGE}" -f ./environments/alpha/Dockerfile .
 
-echo "Pushing Docker image: ${DOCKER_IMAGE}"
-docker push "${DOCKER_IMAGE}"
+if [ -n "${CACHE_FROM}" ]; then
+  echo "Using cache from: ${CACHE_FROM}"
+  docker buildx build --build-arg PORT=${PORT} --cache-from="type=local,src=${CACHE_FROM}" --cache-to="type=local,dest=${CACHE_FROM}" -t "${DOCKER_IMAGE}" -f ./environments/alpha/Dockerfile . --push
+else
+  docker build --build-arg PORT=${PORT} -t "${DOCKER_IMAGE}" -f ./environments/alpha/Dockerfile .
+  echo "Pushing Docker image: ${DOCKER_IMAGE}"
+  docker push "${DOCKER_IMAGE}"
+fi
